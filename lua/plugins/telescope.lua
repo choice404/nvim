ec4f95f099f3adf48374c268b474e655b5643196
choisauce <austinch20@protonmail.com>
2025-01-31 17:59:44 -0800

    Fixed telescope issues for wsl

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,8 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "Telescope",
     keys = {
       -- add a keymap to browse plugin files
       -- stylua: ignore
