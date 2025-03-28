fd473b75308d4690f317b83fae9ff403994a17f4
choice404 <choice404@protonmail.com>
2025-03-27 23:24:01 -0700

    Updated copilot.lua

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,16 +3,16 @@ return {
     "zbirenbaum/copilot.lua",
     config = function()
       require("copilot").setup({
-        suggeestion = {
+        suggestion = {
           enabled = true,
           auto_trigger = true,
           keymap = {
-            accept = "<C-j>",
-            next = "<C-n>",
-            dismis = "<C-d>",
+            accept = "<C-j>", -- Press Ctrl + k to accept a suggestion
+            next = "<C-n>", -- Use Ctrl + j to skip the current suggestion and move on
+            dismiss = "<C-d>", -- Optional: You can dismiss with Ctrl + l if you want
           },
         },
-        panel = { enabled = false },
+        panel = { enabled = false }, -- Disable Copilot's inline suggestions panel
       })
     end,
   },
